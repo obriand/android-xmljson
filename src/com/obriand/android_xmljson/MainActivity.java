d2bbb28f1d986f51e95ea20804ac21498653d63b
Olivier Briand <olivier.briand@orange.com>
2013-01-22 18:39:53 +0100

    Add buttons for xml and json loading

--- src/com/obriand/android_xmljson/MainActivity.java
+++ src/com/obriand/android_xmljson/MainActivity.java
@@ -2,9 +2,14 @@ package com.obriand.android_xmljson;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private static final String TAG = "com.obriand.android_xmljson.MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void mainactivity_load_xml_bt(View view) {
+		Log.i(TAG, "load and parse xml");
+		
+		// Get Count Items from Content Provider
+
+		Toast.makeText(this, "load and parse xml", Toast.LENGTH_LONG).show();
+	}
+	
+	public void mainactivity_load_json_bt(View view) {
+		Log.i(TAG, "load and parse json");
+		
+		// Get Count Items from Content Provider
+
+		Toast.makeText(this, "load and parse json", Toast.LENGTH_LONG).show();
 	}
 
 }
