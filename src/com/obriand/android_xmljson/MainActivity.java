6bc15e21440eb54810ace9a18da350fc088cbdcf
Olivier Briand <olivier.briand@orange.com>
2013-01-23 12:24:13 +0100

    Add xml parsing, ok with stackoverflow rss feed

--- src/com/obriand/android_xmljson/MainActivity.java
+++ src/com/obriand/android_xmljson/MainActivity.java
@@ -1,15 +1,32 @@
 package com.obriand.android_xmljson;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import com.obriand.android_xmljson.StackOverflowXmlParser.Entry;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private static final String TAG = "com.obriand.android_xmljson.MainActivity";
+	
+    private static final String URL = "http://stackoverflow.com/feeds/tag?tagnames=android&sort=newest";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +41,111 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public void mainactivity_load_xml_bt(View view) {
-		Log.i(TAG, "load and parse xml");
-		
-		// Get Count Items from Content Provider
-
+	//Button Get XML
+	public void btGetXML(View view) {
+		Log.i(TAG, "load and parse xml");		
+		loadXmlPage(); // getxml rss feed, transform in html and display in a webview
 		Toast.makeText(this, "load and parse xml", Toast.LENGTH_LONG).show();
 	}
 	
-	public void mainactivity_load_json_bt(View view) {
-		Log.i(TAG, "load and parse json");
-		
+	// Button Get Json
+	public void btGetJson(View view) {
+		Log.i(TAG, "load and parse json");		
 		// Get Count Items from Content Provider
-
 		Toast.makeText(this, "load and parse json", Toast.LENGTH_LONG).show();
 	}
+	
+    // Uses AsyncTask subclass to download the XML feed from stackoverflow.com.
+    // This avoids UI lock up. To prevent network operations from
+    // causing a delay that results in a poor user experience, always perform
+    // network operations on a separate thread from the UI.
+    private void loadXmlPage() {
+            new DownloadXmlTask().execute(URL);
+    }
+    
+    // Implementation of AsyncTask used to download XML feed from stackoverflow.com.
+    private class DownloadXmlTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return loadXmlFromNetwork(urls[0]); // get xml from the internet and return the html file in result String
+            } catch (IOException e) {
+                return getResources().getString(R.string.connection_error);
+            } catch (XmlPullParserException e) {
+                return getResources().getString(R.string.xml_error);
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            setContentView(R.layout.activity_main);
+            // Displays the HTML string in the UI via a WebView
+            WebView myWebView = (WebView) findViewById(R.id.mainactivity_webview);
+            myWebView.loadData(result, "text/html", null); // display html in a webview
+        }
+    }
+    
+    // Uploads XML from stackoverflow.com, parses it, and combines it with
+    // HTML markup. Returns HTML string.
+    private String loadXmlFromNetwork(String urlString) throws XmlPullParserException, IOException {
+        InputStream stream = null;
+        StackOverflowXmlParser stackOverflowXmlParser = new StackOverflowXmlParser();
+        List<Entry> entries = null;
+        
+        Calendar rightNow = Calendar.getInstance();
+        DateFormat formatter = new SimpleDateFormat("MMM dd h:mmaa");
+
+        // Display or not summary in the feed html result
+        boolean pref = true;
+
+        StringBuilder htmlString = new StringBuilder();
+        htmlString.append("<h3>" + getResources().getString(R.string.page_title) + "</h3>");
+        htmlString.append("<em>" + getResources().getString(R.string.updated) + " " +
+                formatter.format(rightNow.getTime()) + "</em>");
+
+        try {
+            stream = downloadUrl(urlString); // make the http request and get the xml stream here
+            entries = stackOverflowXmlParser.parse(stream); // parse the xml stream and return the entries object (list)
+        // Makes sure that the InputStream is closed after the app is
+        // finished using it.
+        } finally {
+            if (stream != null) {
+                stream.close();
+            }
+        }
+
+        // StackOverflowXmlParser returns a List (called "entries") of Entry objects.
+        // Each Entry object represents a single post in the XML feed.
+        // This section processes the entries list to combine each entry with HTML markup.
+        // Each entry is displayed in the UI as a link that optionally includes
+        // a text summary.
+        for (Entry entry : entries) {
+            htmlString.append("<p><a href='");
+            htmlString.append(entry.link);
+            htmlString.append("'>" + entry.title + "</a></p>");			// transform entries list into html String
+            // If the user set the preference to include summary text,
+            // adds it to the display.
+            if (pref) {
+                htmlString.append(entry.summary);
+            }
+        }
+        return htmlString.toString();
+    }
+    
+    // Given a string representation of a URL, sets up a connection and gets
+    // an input stream.
+    private InputStream downloadUrl(String urlString) throws IOException { 		// make the http request on a url and get the inputstream
+        URL url = new URL(urlString);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setReadTimeout(10000 /* milliseconds */);
+        conn.setConnectTimeout(15000 /* milliseconds */);
+        conn.setRequestMethod("GET");
+        conn.setDoInput(true);
+        // Starts the query
+        conn.connect();
+        InputStream stream = conn.getInputStream();
+        return stream;
+    }
 
 }
